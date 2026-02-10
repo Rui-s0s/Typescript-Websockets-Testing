1dcaf283a08a2138bdfbf8b0c07196bc70f28c7c
Rui Segovia <segovia.rodrigo2002@gmail.com>
2026-02-10 10:44:06 -0300

    docs: Documentacion para posible uso no local

--- src/main.ts
+++ src/main.ts
@@ -17,6 +17,20 @@ async function bootstrap() {
   // Set EJS as the view engine
   app.setViewEngine('ejs');
 
-  await app.listen(4000);
+  await app.listen(4000, '0.0.0.0'); // This for not localhost 
 }
 bootstrap();
+
+/*
+  Modify Windows Defender Firewall to allow incoming connections on port 4000:
+
+  1. Open Windows Defender Firewall with Advanced Security.
+  2. Click on "Inbound Rules" in the left pane.
+  3. Click "New Rule..." in the right pane.
+  4. Select "Port" and click "Next".
+  5. Select "TCP" and specify "4000" in the "Specific local ports" field, then click "Next".
+  6. Choose "Allow the connection" and click "Next".
+  7. Select the network profiles (Domain, Private, Public) where you want to allow the connection, then click "Next".
+  8. Give the rule a name (e.g., "NestJS Chat Server") and click "Finish".
+
+*/
