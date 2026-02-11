ba677d3384256e67e19f5950bb7c1d38fdc818d3
Rui Segovia <segovia.rodrigo2002@gmail.com>
2026-02-11 16:38:18 -0300

    refactor: Controller minimalista

--- src/chat/chat.controller.ts
+++ src/chat/chat.controller.ts
@@ -5,7 +5,6 @@ export class ChatController {
   @Get()
   @Render('index') // Renders views/index.ejs
   getChatPage() {
-    // You can pass variables to EJS here
     return { title: 'NestJS Chat' };
   }
 }
