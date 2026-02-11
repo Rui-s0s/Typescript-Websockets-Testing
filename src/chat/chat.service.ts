f31beae548a53f7698c32a4d20f7b64dcab4bf73
Rui Segovia <segovia.rodrigo2002@gmail.com>
2026-02-11 18:12:20 -0300

    test: Cambios para green en test de gateway

--- src/chat/chat.service.ts
+++ src/chat/chat.service.ts
@@ -4,6 +4,33 @@ import { SendMessageDto } from './dto/send-message.dto';
 export class ChatService {
   // Use the Interface for internal storage blueprint
   private readonly messages: ChatMessage[] = [];
+  private rooms = new Map<string, Set<string>>();
+  private socketMap = new Map<string, { room: string; username: string }>();
+
+  addUserToRoom(room: string, username: string, socketId: string) {
+    if (!this.rooms.has(room)) {
+      this.rooms.set(room, new Set());
+    }
+
+    this.rooms.get(room)!.add(username);
+    this.socketMap.set(socketId, { room, username });
+  }
+
+  removeUserBySocket(socketId: string): string | null {
+    const entry = this.socketMap.get(socketId);
+    if (!entry) return null;
+
+    const { room, username } = entry;
+
+    this.rooms.get(room)?.delete(username);
+    this.socketMap.delete(socketId);
+
+    return room;
+  }
+
+  getUserCount(room: string): number {
+    return this.rooms.get(room)?.size ?? 0;
+  }
 
   prepareMessage(dto: SendMessageDto): ChatMessage {
     const newMessage: ChatMessage = {
@@ -14,5 +41,7 @@ export class ChatService {
     this.messages.push(newMessage);
     return newMessage;
   }
+
+
 }
 
