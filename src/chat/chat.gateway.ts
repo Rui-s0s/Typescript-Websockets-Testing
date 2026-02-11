b9814a582d35cb8348a58b1bf5d7d32beaf05b5f
Rui Segovia <segovia.rodrigo2002@gmail.com>
2026-02-11 18:10:25 -0300

    test: GREEN para test de gateway

--- src/chat/chat.gateway.ts
+++ src/chat/chat.gateway.ts
@@ -4,22 +4,39 @@ import {
   MessageBody,
   ConnectedSocket,
   WebSocketServer,
+  OnGatewayConnection,
+  OnGatewayDisconnect,
 } from '@nestjs/websockets';
-import { UsePipes, ValidationPipe } from '@nestjs/common';
 import { Server, Socket } from 'socket.io';
 import { ChatService } from './chat.service';
 import {
   JoinRoomPayload,
-  SendMessagePayload,
 } from './chat.types';
 import { SendMessageDto } from './dto/send-message.dto';
 
-@WebSocketGateway({cors: { origin: '*' }})
-export class ChatGateway {
+@WebSocketGateway({ cors: { origin: '*' } })
+export class ChatGateway
+  implements OnGatewayConnection, OnGatewayDisconnect
+{
   @WebSocketServer()
   server!: Server;
 
   constructor(private readonly chatService: ChatService) {}
+
+  handleConnection(client: Socket) {
+    console.log(`Client connected: ${client.id}`);
+  }
+
+  handleDisconnect(client: Socket) {
+  const room = this.chatService.removeUserBySocket(client.id);
+
+  if (!room) return;
+
+  const count = this.chatService.getUserCount(room);
+
+  this.server.to(room).emit('userCount', { count });
+}
+
 
   @SubscribeMessage('joinRoom')
   handleJoinRoom(
@@ -28,14 +45,18 @@ export class ChatGateway {
   ) {
     client.join(payload.room);
 
-    client.emit('joinedRoom', payload.room);
-  }
+    this.chatService.addUserToRoom(payload.room, payload.username, client.id);
 
-  // This activates the validation
+    const count = this.chatService.getUserCount(payload.room);
+
+    this.server.to(payload.room).emit('userCount', { count });
+  }    
+
   @SubscribeMessage('sendMessage')
   handleMessage(@MessageBody() data: SendMessageDto) {
-    // If the data doesn't match the DTO, this code won't even run!
-    this.server.to(data.room).emit('newMessage', data);
-  }
-}
 
+    const preparedMsg = this.chatService.prepareMessage(data);
+
+    this.server.to(data.room).emit('newMessage', preparedMsg);
+
+}}
