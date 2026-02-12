1954c1452881c2e15718f51d6bde91bec6673a23
Rui Segovia <segovia.rodrigo2002@gmail.com>
2026-02-12 13:55:53 -0300

    refactor: Imports a app.module para funcionamiento de mejoras

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,12 @@
 // app.module.ts
 import { Module } from '@nestjs/common';
-import { ChatController } from './chat/chat.controller'; // Adjust path
+import { ChatController } from './chat/chat.controller'; 
 import { ChatGateway } from './chat/chat.gateway';
 import { ChatService } from './chat/chat.service';
+import { EventEmitterModule } from '@nestjs/event-emitter';
 
 @Module({
-  imports: [],
+  imports: [EventEmitterModule.forRoot()],
   controllers: [ChatController], 
   providers: [ChatGateway, ChatService],
 })
